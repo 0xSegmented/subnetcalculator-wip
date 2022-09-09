7490b481db38075f9a5f1eb23d9980b36aaa8fd9
xatt <111096156+MaliciousXatt@users.noreply.github.com>
2022-09-09 13:04:05 +0200

    Add files via upload

--- SubnetCalc2.py
+++ SubnetCalc2.py
@@ -3,8 +3,8 @@ ip = input("Enter IP\n")
 ip_list = ip.split('.')     # print(ip_list[0])
 int_ip_list = [int(x) for x in ip_list]
 print(int_ip_list)
-if int_ip_list[0] >= 10 and int_ip_list[0] <= 126:
-    print("Class A")
+if int_ip_list[0] >= 0 and int_ip_list[0] <= 126: # Half-Duplex CLASS A
+    print("Class A")   # Subnet kiezen \/
     subnetA = int(input("Choose subnet: 8, 9, 10, 11, 12, 13, 14, 15\n"))
     if subnetA == 8:      
         print("255.0.0.0")
@@ -24,5 +24,61 @@ if int_ip_list[0] >= 10 and int_ip_list[0] <= 126:
         print("255.254.0.0")
     else:
         print("Choose between 8 and 15")
+elif int_ip_list[0] >= 128 and int_ip_list[0] <= 191:
+    print("Class B")
+    subnetB = int(input("Choose subnet: 16, 17, 18, 19, 20, 21, 22, 23\n"))
+    if subnetB == 16:
+        print("255.255.0.0")
+    elif subnetB == 17:
+        print("255.255.128.0")
+    elif subnetB == 18:
+        print("255.255.192.0")
+    elif subnetB == 19:
+        print("255.255.224.0")
+    elif subnetB == 20:
+        print("255.255.240.0")
+    elif subnetB == 21:
+        print("255.255.248.0")
+    elif subnetB == 22:
+        print("255.255.252.0")
+    elif subnetB == 23:
+        print("255.255.254.0")
+    else:
+        print("Choose between 16 and 23")
+
+# IP in stukken bijv: 192 168 1 0
+# Subnet los: 255 255 255 0
+elif int_ip_list[0] >= 192 and int_ip_list[0] <= 223:
+    print("Class C")
+    subnetC = int(input("Choose subnet: 24, 25, 26, 27 28\n"))
+    if subnetC == 24:
+        st = "255.255.255.0"
+        sp = st.split('.')
+        xs = [int(x) for x in sp]
+        print(xs)
+    elif subnetC == 25:
+        st = "255.255.255.128"
+        sp = st.split('.')
+        xs = [int(x) for x in sp]
+        print(xs)
+    elif subnetC == 26:
+        st = "255.255.255.192"
+        sp = st.split('.')
+        xs = [int(x) for x in sp]
+        print(xs)
+    elif subnetC == 27:
+        st = "255.255.255.224"
+        sp = st.split('.')
+        xs = [int(x) for x in sp]
+        print(xs)
+    elif subnetC == 28:
+        st = "255.255.255.240"
+        sp = st.split('.')
+        xs = [int(x) for x in sp]
+        print(xs)
+    else:
+        print("Choose between 24 and 28")
+
+
 else:
     print("fail")
